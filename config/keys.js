bfa9e46a64d5d5a3f9b473e61eb297dc2f689179
Abdullah <abdullah.salem72@gmail.com>
2018-05-11 07:57:31 +0800

    heroku

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://admin:root@ds119650.mlab.com:19650/stroy_book_dev",
+  googleClientID:
+    "238074817177-7iaq7e40tobaf20bql45id4m3v901fok.apps.googleusercontent.com",
+  googleClientSecret: "9t5U5_Ls19_ewGxeAN20lpHn"
+};
